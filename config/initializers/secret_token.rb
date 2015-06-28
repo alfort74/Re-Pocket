bf8a198d35f0d1ef2c03cebb0a77410878704d9e
alfort74 <alfort625@gmail.com>
2015-06-28 11:59:54 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PocketSandbox::Application.config.secret_key_base = '3fe211740fa51fb7a5b217842bd65689892f5285c009915a0d03b6fafff6d7e9f1ee571ecdb2400a6f6d8f21b73587da3003de4a08addd140c07de1a467b0157'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PocketSandbox::Application.config.session_store :cookie_store, key: '_pocket_sandbox_session'
